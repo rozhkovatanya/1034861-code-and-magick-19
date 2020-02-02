f3bb2a68af2bb5725930daeddb893c765f8725b5
Tetyana Rozhkova <tetyanarozhkova@Tetyanas-MacBook-Air.local>
2020-02-02 20:09:11 +0100

    Задание 1 модуля 3 готово

---
+++ js/setup.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
+
+var WIZARD_NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+
+var WIZARD_SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+
+var WIZARD_COAT = [
+  'rgb (101, 137, 164)',
+  'rgb (241, 43, 107)',
+  'rgb (146, 100, 161)',
+  'rgb (56, 159, 117)',
+  'rgb (215, 210, 55)',
+  'rgb (0, 0, 0)'
+];
+
+var WIZARD_EYES = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+
+var getRandomElement = function (array) {
+  var randomNumber = Math.random() * array.length;
+  var index = Math.floor(randomNumber);
+  return array[index];
+};
+
+var wizards = [];
+for (var i = 0; i < 4; i++) {
+  wizards.push({
+    name: getRandomElement(WIZARD_NAMES),
+    surname: getRandomElement(WIZARD_SURNAMES),
+    coatColor: getRandomElement(WIZARD_COAT),
+    eyesColor: getRandomElement(WIZARD_EYES)
+  });
+}
+
+for (var i = 0; i < wizards.length; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name + ' ' + wizards[i].surname;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+
+  similarListElement.appendChild(wizardElement);
+}
